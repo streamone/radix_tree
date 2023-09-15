c546e297ef0772f0f757070c04e574cdf7b135ab
streamone <streamone@163.com>
2023-09-15 15:20:42 +0800

    fix compile warnings about "std::iterator deprecated"

--- radix_tree_it.hpp
+++ radix_tree_it.hpp
@@ -9,10 +9,16 @@ template <typename K, typename T, class Compare = std::less<K> > class radix_tre
 template <typename K, typename T, class Compare = std::less<K> > class radix_tree_node;
 
 template <typename K, typename T, class Compare = std::less<K> >
-class radix_tree_it : public std::iterator<std::forward_iterator_tag, std::pair<K, T> > {
+class radix_tree_it {
     friend class radix_tree<K, T, Compare>;
 
 public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = std::pair<K, T>;
+    using difference_type = std::ptrdiff_t;
+    using pointer = const std::pair<K, T>*;
+    using reference = const std::pair<K, T>&;
+
     radix_tree_it() : m_pointee(0) { }
     radix_tree_it(const radix_tree_it& r) : m_pointee(r.m_pointee) { }
     radix_tree_it& operator=(const radix_tree_it& r) { m_pointee = r.m_pointee; return *this; }
@@ -116,3 +122,4 @@ const radix_tree_it<K, T, Compare>& radix_tree_it<K, T, Compare>::operator-- ()
 */
 
 #endif // RADIX_TREE_IT
+
